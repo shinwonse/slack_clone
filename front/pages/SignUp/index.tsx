edb8bd61657223fd18a92608774f613a3f2b55cd
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-14 23:15:52 +0900

    feat: 회원가입 페이지 구현

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import useInput from '@hooks/useInput';
 import axios from 'axios';
-import React, { useCallback, useState, VFC } from 'react';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Form, Error, Label, Input, LinkContainer, Button, Header, Success } from './styles';
 
 const SignUp = () => {
@@ -98,7 +99,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
